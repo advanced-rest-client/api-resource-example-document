74a150aa066a39cd77e2c13087d4216981cdb19b
Pawel Psztyc <jarrodek@gmail.com>
2018-12-27 15:15:10 -0800

    Updating component to work with XML types

--- demo/model.js
+++ demo/model.js
@@ -1,7 +1,8 @@
 const generator = require('@api-components/api-model-generator');
 
 const files = new Map();
-files.set('exchange-experience-api/exchange-experience-api.raml', 'RAML 0.8');
+files.set('demo-api/demo-api.raml', 'RAML 1.0');
+files.set('raml-types/raml-types.raml', 'RAML 1.0');
 
 generator(files)
 .then(() => console.log('Finito'));
