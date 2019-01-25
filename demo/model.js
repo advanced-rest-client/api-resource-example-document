261f0af8daa11cb7a21c986e202ee0830e491588
Pawel Psztyc <jarrodek@gmail.com>
2019-01-24 16:17:34 -0800

    Update: Updating project dev dependencies. Bumping version so the changes will be deliver do maaster.

--- demo/model.js
+++ demo/model.js
@@ -1,8 +1,4 @@
 const generator = require('@api-components/api-model-generator');
-
-const files = new Map();
-files.set('demo-api/demo-api.raml', 'RAML 1.0');
-files.set('raml-types/raml-types.raml', 'RAML 1.0');
-
-generator(files)
-.then(() => console.log('Finito'));
+generator('./demo/apis.json')
+.then(() => console.log('Models created'))
+.catch((cause) => console.error(cause));
